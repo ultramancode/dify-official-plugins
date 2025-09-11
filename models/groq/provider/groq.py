527c7b3b06c0d27262914d5b1313e0daac3c3513
crazywoola <100913391+crazywoola@users.noreply.github.com>
2025-09-11 21:30:37 +0800

    fix: groq validation (#1696)

--- models/groq/provider/groq.py
+++ models/groq/provider/groq.py
@@ -16,7 +16,7 @@ class GroqProvider(ModelProvider):
         """
         try:
             model_instance = self.get_model_instance(ModelType.LLM)
-            model_instance.validate_credentials(model="llama3-8b-8192", credentials=credentials)
+            model_instance.validate_credentials(model="grok-code-fast-1-0825", credentials=credentials)
         except CredentialsValidateFailedError as ex:
             raise ex
         except Exception as ex:
